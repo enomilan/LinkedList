7e6a023bf994db03b8cbe9e25d2b50b6d5e830b7
enomilan <andrewmusta@gmail.com>
2021-02-27 23:07:39 -0600

    finished stack

--- stack/lib/stack.js
+++ stack/lib/stack.js
@@ -22,12 +22,60 @@
 // -----------
 
 class Node {
-
+    constructor(value) {
+        this.value = value
+        this.next = null
+    }
 }
 
 class Stack {
 
-}
+    constructor() {
 
+        this.top = null
+        this.length = 0;
+    }
+
+
+
+
+push(value) {
+    if (this.top) {
+        const prev = this.top
+        this.top = new Node(value)
+      
+        this.top.next = prev
+        this.length++
+        return this.size()
+
+
+
+    } else if (!this.top) {
+        this.top = new Node(value); 
+        this.length++;
+
+        return this.size()
+    }
+  
+}
+pop() {
+    if (!this.top) return null;
+    else { 
+        const prev = this.top
+        this.top = prev.next
+        
+        this.length--
+        return prev.value;
+    }
+
+   
+}
+size() {
+    if (this.length <= 0) {
+        this.length
+    }
+    return this.length
+}
+}
 exports.Node = Node;
 exports.Stack = Stack;
